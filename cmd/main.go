cadcff4bbb7fddecc001bc2837f7013c034102b1
ykonomi <hgKaguyaToupako4er@gmail.com>
2024-02-17 13:36:50 +0900

    add

--- cmd/main.go
+++ cmd/main.go
@@ -41,9 +41,9 @@ func main() {
 		i := item{}
 		i.name = a[0]
 		i.price, _ = strconv.Atoi(a[1])
-		//		i.sell = int(float32(i.buy) * 0.8)
+		//		i.sell =
 		//		i.bless = int(float32(i.buy) * 2.0)
-		//		i.curse = int(float32(i.buy) * 0.87)
+		//		i.curse =
 
 		items = append(items, i)
 	}
@@ -58,11 +58,68 @@ func main() {
 	// }
 	// defer fp1.Close()
 
-	fmt.Println("var list = map[int]string{")
+	// fmt.Println("var list = map[int]string{")
+	var buy = map[int][]string{}
+	var sell = map[int][]string{}
 	for _, i := range items {
-		fmt.Printf(fmt.Sprintf("%d: \"%s\",\n", i.price, i.name))
+		switch {
+		case strings.Contains(i.name, "壺"):
+			for j := 0; j < 7; j++ {
+				n := fmt.Sprintf("[%d]", j)
+				b := i.price + j*100
+				// normal
+				buy[b] = append(buy[b], i.name+n)
+				// bless
+				buy[b*2] = append(buy[b*2], i.name+n+"[祝]")
+				// curse
+				buy[int(float32(b)*0.87)] = append(buy[int(float32(b)*0.87)], i.name+n+"[呪]")
+
+				p := int(float32(b) * 0.8)
+				// normal
+				sell[p] = append(sell[p], i.name+n)
+				// bless
+				sell[p*2] = append(sell[p*2], i.name+n+"[祝]")
+				// curse
+				sell[int(float32(p)*0.87)] = append(sell[int(float32(p)*0.87)], i.name+n+"[呪]")
+			}
+		case strings.Contains(i.name, "杖"):
+			for j := 0; j < 8; j++ {
+				n := fmt.Sprintf("[%d]", j)
+				b := i.price + j*100
+				// normal
+				buy[b] = append(buy[b], i.name+n)
+				// bless
+				buy[b*2] = append(buy[b*2], i.name+n+"[祝]")
+				// curse
+				buy[int(float32(b)*0.87)] = append(buy[int(float32(b)*0.87)], i.name+n+"[呪]")
+
+				p := int(float32(b) * 0.8)
+				// normal
+				sell[p] = append(sell[p], i.name+n)
+				// bless
+				sell[p*2] = append(sell[p*2], i.name+n+"[祝]")
+				// curse
+				sell[int(float32(p)*0.87)] = append(sell[int(float32(p)*0.87)], i.name+n+"[呪]")
+			}
+		default:
+			// normal
+			buy[i.price] = append(buy[i.price], i.name)
+			// bless
+			buy[i.price*2] = append(buy[i.price*2], i.name+"[祝]")
+			// curse
+			buy[int(float32(i.price)*0.87)] = append(buy[int(float32(i.price)*0.87)], i.name+"[呪]")
+
+			p := int(float32(i.price) * 0.8)
+			// normal
+			sell[p] = append(sell[p], i.name)
+			// bless
+			sell[p*2] = append(sell[p*2], i.name+"[祝]")
+			// curse
+			sell[int(float32(p)*0.87)] = append(sell[int(float32(p)*0.87)], i.name+"[呪]")
+		}
 	}
-	fmt.Println("}")
+	// fmt.Println("}")
+	fmt.Println(buy)
 }
 
 func open(filename string) {
