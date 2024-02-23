d308c4c1d4f68056cc6e2e5e090ce36fe1909c6b
ykonomi <hgKaguyaToupako4er@gmail.com>
2024-02-23 22:45:58 +0900

    refactoring

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,15 +1,29 @@
 import { PageProps } from "$fresh/server.ts";
+import { Handlers } from "$fresh/server.ts";
 import Display from "../islands/Display.tsx";
+import { Partial } from "$fresh/runtime.ts";
 
+interface Project {
+  content: Uint8Array;
+}
 
-export default function Home(context: PageProps) {
-  const wasmCode = Deno.readFileSync("./main.wasm");
+export const handler: Handlers<Project> = {
+  async GET(_req, ctx) {
+    const content = await Deno.readFile("./main.wasm");
+    return ctx.render({content: content});
+  },
+};
+
+export default function Home(props: PageProps) {
 
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
-      <div class="gap-4 max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <Display wasm={wasmCode} />
+    <Partial name="docs-content">
+      <div class="px-4 py-8 mx-auto bg-[#86efac]">
+        <div class="gap-4 max-w-screen-md mx-auto flex flex-col items-center justify-center">
+          <Display wasm={props.data.content} />
+        </div>
       </div>
-    </div>
-  );
+    </Partial>
+  )
 }
+
